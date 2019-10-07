28c81653d88ed40ed9f4c2077363185d2ddc4992
JD Morise <48957270+jdmorise@users.noreply.github.com>
2019-10-07 22:26:56 +0200

    Upload of additional fonts for digits

---
+++ bmh_fonts/bmh_digits/Just Another Hand Regular/Just Another Hand Regular_24.h
@@ -0,0 +1,17 @@
+// Header File for SSD1306 characters
+// Generated with TTF2BMH
+// Font Just Another Hand Regular
+// Font Size: 24
+const char bitmap_48[] = {255,255,1,0,3,255,252,15,127,224,192,240,127,15,0,0,0,0,0,0,0};
+const char bitmap_49[] = {1,255,224,0,127,127,0,0,0};
+const char bitmap_50[] = {6,7,1,0,224,255,31,0,192,248,255,207,193,192,0,0,0,0,0,0,0};
+const char bitmap_51[] = {1,48,56,30,55,241,192,56,120,96,96,112,63,15,0,0,0,0,0,0,0};
+const char bitmap_52[] = {128,240,126,7,255,252,128,128,1,1,1,1,63,127,1,1,0,0,0,0,0,0,0,0};
+const char bitmap_53[] = {1,3,1,1,3,255,252,60,120,96,96,120,63,7,0,0,0,0,0,0,0};
+const char bitmap_54[] = {248,255,59,24,56,240,192,15,63,112,96,96,63,31,0,0,0,0,0,0,0};
+const char bitmap_55[] = {0,248,255,3,112,127,15,0,0,0,0,0};
+const char bitmap_56[] = {143,255,112,56,127,239,128,63,127,192,192,224,127,31,0,0,0,0,0,0,0};
+const char bitmap_57[] = {127,255,192,128,192,255,255,0,0,1,97,125,31,1,0,0,0,0,0,0,0};
+const char bitmap_58[] = {192,192,96,96,0,0};
+const char char_width[] = {7,3,7,7,8,7,7,4,7,7,2};
+const char* char_addr[] = {&bitmap_48,&bitmap_49,&bitmap_50,&bitmap_51,&bitmap_52,&bitmap_53,&bitmap_54,&bitmap_55,&bitmap_56,&bitmap_57,&bitmap_58};
---
+++ bmh_fonts/bmh_digits/jsMath-cmex10/jsMath-cmex10_24.h
@@ -0,0 +1,17 @@
+// Header File for SSD1306 characters
+// Generated with TTF2BMH
+// Font jsMath-cmex10
+// Font Size: 24
+const char bitmap_48[] = {0,0,0,0,128,224};
+const char bitmap_49[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,3,14,56,240,192,128};
+const char bitmap_50[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,255,255,3,3,3,3,3,3,3};
+const char bitmap_51[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,3,3,3,3,3,3,255,255};
+const char bitmap_52[] = {0,0,0,0,255,255};
+const char bitmap_53[] = {0,0,0,0,255,255};
+const char bitmap_54[] = {0,0,0,0,254,254};
+const char bitmap_55[] = {0,0,0,0,254,254};
+const char bitmap_56[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,128,224,240,120,28,12,6};
+const char bitmap_57[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,6,14,28,120,240,224,128};
+const char bitmap_58[] = {0,0,0,0,0,0,254,254,254};
+const char char_width[] = {2,7,9,9,2,2,2,2,7,8,3};
+const char* char_addr[] = {&bitmap_48,&bitmap_49,&bitmap_50,&bitmap_51,&bitmap_52,&bitmap_53,&bitmap_54,&bitmap_55,&bitmap_56,&bitmap_57,&bitmap_58};
